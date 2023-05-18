805ed18eb651c6d4347380f56689d1ed958fdd6d
william arasirwa <mageboarasirwa03@gmail.com>
2023-05-18 11:05:08 +0300

    complete

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+    def talk
+
+        puts "Hello World!"
+
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+
+
+
+end
+
+william = Person.new()
+
+william.talk
+william.walk
